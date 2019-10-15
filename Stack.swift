ce12a68cdce0dc394864934e74a1fb09b0d8799c
jh1jia <jh2jia@edu.uwaterloo.ca>
2019-10-14 23:54:22 -0400

    add

--- Stack.swift
+++ Stack.swift
@@ -51,3 +51,26 @@ extension Stack: ExpressibleByArrayLiteral {
         storage = elements
     }
 }
+
+
+var testString1 = "h((e))llo(world)()"
+//var testString2 = "(hello world"
+
+func checkParentheses(_ string: String) -> Bool {
+    var stack = Stack<Character>()
+    
+    for character in string {
+        if character == "(" {
+            stack.push(character)
+        } else if character == ")" {
+            if stack.isEmpty {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+    }
+    return stack.isEmpty
+}
+
+checkParentheses(testString1)
