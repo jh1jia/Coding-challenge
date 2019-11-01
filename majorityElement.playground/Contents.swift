a40f7095378373dd497355dc875f0f6e34d7deea
jh1jia <jh2jia@edu.uwaterloo.ca>
2019-11-01 19:11:22 -0400

    add

--- majorityElement.playground/Contents.swift
+++ majorityElement.playground/Contents.swift
@@ -1,3 +1,9 @@
-import UIKit
-
-var str = "Hello, playground"
+class Solution {
+    
+    func majorityElement(_ nums: [Int]) -> Int {
+        var nums = nums
+        nums.sort { $0 < $1 }
+        return nums[nums.count / 2]
+    }
+    
+}
