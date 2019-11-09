3bdd74180d9736307da8c96a0f76456e3a020c94
jh1jia <jh2jia@edu.uwaterloo.ca>
2019-11-08 19:27:11 -0500

    add

---
+++ containsNearbyDuplicate.swift
@@ -0,0 +1,17 @@
+func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+    guard nums.count > 1 else {
+        return false
+    }
+    
+    var numToLastIndex = [Int: Int]()
+    
+    for (i, num) in nums.enumerated() {
+        if let lastIndex = numToLastIndex[num], i - lastIndex <= k {
+            return true
+        } else {
+            numToLastIndex[num] = i
+        }
+    }
+    
+    return false
+}
